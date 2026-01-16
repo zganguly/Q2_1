6e10d61905d6a31488dc8cee47679ab89e03a0d4
Z Ganguly <gangulymyname@gmail.com>
2026-01-16 20:53:11 +0530

    Simplified Code

--- src/apiResponse.ts
+++ src/apiResponse.ts
@@ -1,23 +1,29 @@
+
 export type ApiSuccess<T> = {
-    success: true;
-    data: T;
-  };
-  
-  export type ApiError = {
-    success: false;
-    error: string;
-  };
-  
-  export type ApiResponse<T> = ApiSuccess<T> | ApiError;
-  
-  export function createApiResponse<T>(
-    data: T,
-    error?: string
-  ): ApiResponse<T> {
-    if (error) {
-      return { success: false, error };
-    }
-  
-    return { success: true, data };
+  success: true;
+  data: T;
+};
+
+
+export type ApiError = {
+  success: false;
+  error: string;
+};
+
+
+export type ApiResponse<T> = ApiSuccess<T> | ApiError;
+
+
+export function createApiResponse<T>(data: T, error?: string): ApiResponse<T> {
+  if (error !== undefined && error !== null) {
+    return {
+      success: false,
+      error: error
+    };
+  } else {
+    return {
+      success: true, 
+      data: data  
+    };
   }
-  
+}
